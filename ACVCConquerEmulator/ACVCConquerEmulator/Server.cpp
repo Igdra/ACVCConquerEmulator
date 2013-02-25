70126c1cd668d84c1fde0a87573cda749b8683b4
Antonio Carlos Vaccaro Carvalho <acvcnos@hotmail.com>
2013-02-25 14:30:42 -0300

    Still working on socket system

--- ACVCConquerEmulator/ACVCConquerEmulator/Server.cpp
+++ ACVCConquerEmulator/ACVCConquerEmulator/Server.cpp
@@ -23,21 +23,30 @@ public:
 		Listener = gcnew Socket(AddressFamily::InterNetwork,
 			SocketType::Stream, ProtocolType::Tcp);
 		Listener->Bind(gcnew IPEndPoint(InternalIP, InternalPort));
-	};
+	}
 public:
+	void RecieveData( IAsyncResult^ Result)
+	{
+		SyncObj^ Mediator = (SyncObj^)Result->AsyncState;
+
+	}
 	void RecieveConnection( IAsyncResult^ Result)
 	{
 		SyncObj^ Mediator = (SyncObj^)Result->AsyncState;
 		Mediator->Client = Listener->EndAccept(Result);
 		Mediator->Buffer = gcnew array<unsigned char>(65535);
 
+		Listener->BeginReceive(Mediator->Buffer, 0, 65535, SocketFlags::None,
+			gcnew AsyncCallback(this, &Server::RecieveData), Mediator);
+
+		OnConnect(Mediator);
 		Listener->BeginAccept(gcnew AsyncCallback(this, &Server::RecieveConnection) , gcnew SyncObj() );
-	};
+	}
 public:
 	void Start()
 	{
 		Listener->Listen(5000);
 		Listener->BeginAccept(gcnew AsyncCallback(this, &Server::RecieveConnection) , gcnew SyncObj() );
-	};
+	}
 
 };
