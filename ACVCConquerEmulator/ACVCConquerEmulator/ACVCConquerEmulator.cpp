55d8aad16462336d1e2bdcbd995a64a6f843d816
Antonio Carlos Vaccaro Carvalho <acvcnos@hotmail.com>
2013-02-26 15:44:48 -0300

    Preparation for password crypt support

--- ACVCConquerEmulator/ACVCConquerEmulator/ACVCConquerEmulator.cpp
+++ ACVCConquerEmulator/ACVCConquerEmulator/ACVCConquerEmulator.cpp
@@ -16,8 +16,8 @@ public:
 	static void AuthConnectAction(SyncObj^ Mediator)
 	{
 #ifdef DEBUG
-		printf("Connection recieved : "+Mediator->Client->RemoteEndPoint->ToString()+"\n");
-		printf("Sending seed.. ");
+		printf("[DEBUG]Connection recieved : "+Mediator->Client->RemoteEndPoint->ToString()+".\n");
+		printf("[DEBUG]Sending seed.. ");
 #endif
 		unsigned int Seed = Ultilities::RandomUInt32();
 		Client^ ConnectingClient = gcnew Client();
@@ -43,14 +43,29 @@ public:
             case 1086:
 				{
 #ifdef DEBUG
-					printf("Login requested.\n");
+					printf("[DEBUG]Login requested.\n");
 #endif
+					String^ User = Ultilities::ReadString(buffer, 4, 16)->TrimEnd('\0');
+#ifdef DEBUG
+					printf("[DEBUG]User name found : '"+ User+"'.\n");
+#endif
+					array<unsigned char>^ RawPassword = gcnew array<unsigned char>(16);
+					Buffer::BlockCopy(buffer, 132, RawPassword, 0, 16);
+					msvcrt::msvcrt::srand((int)MyClient->PasswordSeed);
+					array<unsigned char>^ rc5Key = gcnew array<unsigned char>(16);
 
+					for (int i = 0; i < 16; i++)
+						rc5Key[i] = (unsigned char)msvcrt::msvcrt::rand();
+
+					/* TODO : Add password crypt. support.. (Prepared for hell?) */
 					break;
 				}
 		}
 	}
 };
+
+
+
 
 
 
--- ACVCConquerEmulator/ACVCConquerEmulator/Ultilities.h
+++ ACVCConquerEmulator/ACVCConquerEmulator/Ultilities.h
@@ -5,4 +5,8 @@ public ref class Ultilities sealed abstract
 public:
 	static Random^ RGen = gcnew Random();
 	static unsigned int RandomUInt32();
+	static String^ ReadString(array<unsigned char>^ buffer, int Offset, int Length)
+	{
+		return System::Text::Encoding::ASCII->GetString(buffer, Offset, Length);
+	}
 };
